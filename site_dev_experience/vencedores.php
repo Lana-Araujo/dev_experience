a8c334a0df096a88b73e2787818d57b74e4715be
Lana Araújo <lana.academico2@gmail.com>
2023-11-17 14:39:28 -0300

    vencedores pt2

--- site_dev_experience/vencedores.php
+++ site_dev_experience/vencedores.php
@@ -25,17 +25,37 @@
     </ul>
   </nav>
 
-      <div class="carousel-item"><img src="img/dev.png" alt="Imagem 1" class="img1"></div>
-  <div class="vencedores-content">
+      <div class="carousel-item"><img src="dev.png" alt="Imagem 1" class="img1"></div>
+      <div class="vencedores-content">
     <h2>Vencedores da DEV Experience</h2>
-    <p>Aqui estão as equipes vencedoras:</p>
-    <ul>
-      <li>Troféu: Nome da equipe</li>
-      <li>Medalha de ouro: Nome da equipe</li>
-      <li>Medalha de prata: Nome da equipe</li>
-      <li>Medalha de Bronze: Nome da equipe</li>
-      <!-- Adicione mais categorias e vencedores conforme necessário -->
-    </ul>
+
+    <!-- Troféu -->
+    <section>
+      <h3>Troféu</h3>
+      <img src="trofeu_image.jpg" alt="Troféu" class="trofeu-image">
+    </section>
+
+    <!-- Medalha de Ouro -->
+    <section>
+      <h3>Medalha de Ouro</h3>
+      <p>Equipe vencedora da Medalha de Ouro - 2023</p>
+      <img src="ouro_image.jpg" alt="Medalha de Ouro" class="ouro-image">
+    </section>
+
+    <!-- Medalha de Prata -->
+    <section>
+      <h3>Medalha de Prata</h3>
+      <p>Equipe vencedora da Medalha de Prata - 2023</p>
+      <img src="prata_image.jpg" alt="Medalha de Prata" class="prata-image">
+    </section>
+
+    <!-- Medalha de Bronze -->
+    <section>
+      <h3>Medalha de Bronze</h3>
+      <p>Equipe vencedora da Medalha de Bronze - 2023</p>
+      <img src="bronze_image.jpg" alt="Medalha de Bronze" class="bronze-image">
+    </section>
+
   </div>
 
 </body>
