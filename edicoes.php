5baadb3274fe0d17888aec150c2942c017435e4e
Lana Araújo <lana.academico2@gmail.com>
2023-11-17 15:00:05 -0300

    edicoes

---
+++ edicoes.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="edicoes.css">
+  <title>Sua Página</title>
+</head>
+<body>
+<nav>
+    <img src="logodev.png" class="logo">
+    <ul>
+      <li><a href="index.html">Módulos</a></li>
+      <li class="dropdown">
+        <a href="#" class="dropbtn">Edições</a>
+        <div class="dropdown-content">
+          <a href="edicoes.php">Edição 2023</a>
+        </div>
+      </li>
+      <li><a href="precos.html">Galeria</a></li>
+      <li><a href="servicos.html">Vencedores</a></li>
+    </ul>
+  </nav>
+  <img src="2023.png" alt="Imagem 1" class="img1"></div>
+  <section>
+    <p>
+        O "SENAI Dev Experience" é uma maratona de tecnologia que reúne estudantes de nível técnico da Escola e Faculdade SENAI Felix Guisard em uma competição de aprendizado prático, interação entre estudantes e incentivo ao trabalho em equipe para resolução de problemas.
+    </p>
+    <p>
+        A competição do evento foi dividida em três grandes módulos.    </p>
+    <p>
+        O "SENAI Dev Experience" representa mais do que uma simples competição; trata-se de uma imersão profunda no universo da tecnologia, concebida para potencializar o aprendizado e a interação colaborativa entre estudantes. Este evento anual não só desafia a habilidade técnica dos participantes, mas também os inspira a transcender limites, promovendo o espírito inovador e a capacidade de, em equipe, enfrentar desafios do mercado de trabalho.
+    </p>
+</section>
+<h2> Confira nossas fotos:</h2>
+<img src="#" alt="" srcset="">
+<img src="#" alt="" srcset="">
+
+</body>
+</html>
