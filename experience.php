b513b1d2b6f1c1e8f0e6214b0956c035ebdc06a0
marianeleticia <souzamoreiramarianeleticia@gmail.com>
2023-11-17 14:39:50 -0300

    experience

---
+++ experience.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="experience.css">
+    <title>Página de Entrada</title>
+</head>
+
+    <title>Página de Entrada</title>
+</head>
+<body>
+    <div class="container">
+        <div class="left-side">
+            <img src="logo-dev.png" alt="Logo" />
+            <h1>Seja Bem-Vindo!</h1>
+            <!-- <p>Conheça mais sobre o primeiro ano do evento "SENAI Dev Experience",
+            uma maratona de tecnologia que reúne estudantes de nível técnico da
+            Escola e Faculdade SENAI Felix Guisard
+            </p> -->
+            <div class="button-container">
+                <button class="button">Página Inicial</button>
+                <button class="button">Inscreva-se</button>
+            </div>
+        </div>
+        <div class="right-side">
+            <video autoplay muted loop>
+                <source src="video-dev.mp4" type="video/mp4">
+                Seu navegador não suporta o elemento de vídeo.
+            </video>
+        </div>
+    </div>
+</body>
+</html> 
