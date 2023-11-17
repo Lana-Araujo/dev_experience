c3e76cf8318630b6b6b275ea2da77d81acfd3121
Lana Araújo <lana.academico2@gmail.com>
2023-11-17 14:23:44 -0300

    vencedores

---
+++ site_dev_experience/vencedores.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="vencedores.css">
+  <title>DEV Experience</title>
+</head>
+
+<body>
+
+  <nav>
+    <img src="logodev.png" class="logo">
+    <ul>
+      <li><a href="index.html">Módulos</a></li>
+      <li class="dropdown">
+        <a href="#" class="dropbtn">Edições</a>
+        <div class="dropdown-content">
+          <a href="edicoes.php">Edição 2023</a>
+        </div>
+      </li>
+      <li><a href="precos.html">Galeria</a></li>
+      <li><a href="servicos.html">Vencedores</a></li>
+    </ul>
+  </nav>
+
+      <div class="carousel-item"><img src="img/dev.png" alt="Imagem 1" class="img1"></div>
+  <div class="vencedores-content">
+    <h2>Vencedores da DEV Experience</h2>
+    <p>Aqui estão as equipes vencedoras:</p>
+    <ul>
+      <li>Troféu: Nome da equipe</li>
+      <li>Medalha de ouro: Nome da equipe</li>
+      <li>Medalha de prata: Nome da equipe</li>
+      <li>Medalha de Bronze: Nome da equipe</li>
+      <!-- Adicione mais categorias e vencedores conforme necessário -->
+    </ul>
+  </div>
+
+</body>
+
+</html>
